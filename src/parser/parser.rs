990a20153d639c5f2da58126ee5489bdfcfa4745
tavindev <gvwchevrand@gmail.com>
2023-05-31 16:01:56 +0100

    if statements

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -91,31 +91,37 @@ impl Parser {
         match self.lexer.next_token() {
             Token::Int(_) | Token::String(_) | Token::Ident(_) => match self.lexer.peek_token() {
                 Token::Plus | Token::Minus => self.parse_operator_expression(),
-                Token::Semicolon | Token::Eof | Token::Newline => self.parse_literal_expression(),
+                Token::Semicolon
+                | Token::Eof
+                | Token::Newline
+                | Token::RSquirly
+                | Token::Rparen => self.parse_literal_expression(),
                 next_token => panic!("Expected an operator or value, got {:?}", next_token),
             },
-            token => panic!("Expected an operator or value, got {:?}", token),
+            token => panic!("Expected an expression or value, got {:?}", token),
         }
     }
 
     fn parse_block_statement(&mut self) -> BlockStatement {
-        let mut statements = Vec::new();
-
         match self.lexer.next_token() {
-            Token::LSquirly => loop {
-                match self.lexer.next_token() {
-                    Token::Let => statements.push(self.parse_let_statement()),
-                    Token::If => statements.push(self.parse_if_statement()),
-                    Token::RSquirly => break,
-                    _ => {}
+            Token::LSquirly => {
+                let mut statements = Vec::new();
+
+                loop {
+                    match self.lexer.next_token() {
+                        Token::Let => statements.push(self.parse_let_statement()),
+                        Token::If => statements.push(self.parse_if_statement()),
+                        Token::RSquirly => break,
+                        _ => {}
+                    }
                 }
-            },
+
+                return BlockStatement(statements);
+            }
             _ => {
                 panic!("Expected a left brace");
             }
         };
-
-        BlockStatement(statements)
     }
 
     fn parse_if_statement(&mut self) -> Statement {
@@ -267,10 +273,41 @@ mod test {
     }
 
     /**
+     * BLOCK STATEMENT
+     */
+    #[test]
+    fn parse_empty_block_statement() {
+        let input = "{}";
+        let parser = Parser::new(input.into());
+
+        assert_eq!(parser.statements.len(), 0);
+    }
+
+    #[test]
+    fn parse_block_statement_literal() {
+        let input = "{ let x = 3; }";
+        let parser = Parser::new(input.into());
+
+        assert_eq!(parser.statements.len(), 1);
+
+        if let Statement::Block(statement) = &parser.statements[0] {
+            assert_eq!(statement.0.len(), 1);
+
+            if let Statement::Let(let_statement) = &statement.0[0] {
+                assert_eq!(let_statement.name.0, "x");
+                assert_eq!(
+                    let_statement.expression,
+                    Expression::Literal(Value::Int("3".into()))
+                );
+            }
+        }
+    }
+
+    /**
      * IF STATEMENT
      */
     #[test]
-    fn parse_if_statement_literal() {
+    fn parse_if_statement_no_consequence() {
         let input = "if (3) {}";
         let parser = Parser::new(input.into());
 
@@ -283,5 +320,76 @@ mod test {
             );
             assert_eq!(statement.consequence.0.len(), 0);
         }
+    }
+
+    #[test]
+    fn parse_if_statement_no_alternative() {
+        let input = "if (3) { let x = 3; }";
+        let parser = Parser::new(input.into());
+
+        assert_eq!(parser.statements.len(), 1);
+
+        if let Statement::If(statement) = &parser.statements[0] {
+            assert_eq!(
+                statement.condition,
+                Expression::Literal(Value::Int("3".into()))
+            );
+            assert_eq!(statement.consequence.0.len(), 1);
+
+            if let Statement::Let(let_statement) = &statement.consequence.0[0] {
+                assert_eq!(let_statement.name.0, "x");
+                assert_eq!(
+                    let_statement.expression,
+                    Expression::Literal(Value::Int("3".into()))
+                );
+            }
+
+            assert_eq!(statement.alternative.is_none(), true);
+        }
+    }
+
+    #[test]
+    fn parse_if_statement_alternative() {
+        let input = "if (3) { let x = 3; } else { let y = 4; }";
+        let parser = Parser::new(input.into());
+
+        assert_eq!(parser.statements.len(), 1);
+
+        if let Statement::If(statement) = &parser.statements[0] {
+            assert_eq!(
+                statement.condition,
+                Expression::Literal(Value::Int("3".into()))
+            );
+            assert_eq!(statement.consequence.0.len(), 1);
+
+            if let Statement::Let(let_statement) = &statement.consequence.0[0] {
+                assert_eq!(let_statement.name.0, "x");
+                assert_eq!(
+                    let_statement.expression,
+                    Expression::Literal(Value::Int("3".into()))
+                );
+            }
+
+            assert_eq!(statement.alternative.is_some(), true);
+
+            if let Some(alternative) = &statement.alternative {
+                assert_eq!(alternative.0.len(), 1);
+
+                if let Statement::Let(let_statement) = &alternative.0[0] {
+                    assert_eq!(let_statement.name.0, "y");
+                    assert_eq!(
+                        let_statement.expression,
+                        Expression::Literal(Value::Int("4".into()))
+                    );
+                }
+            }
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_if_statement_no_condition() {
+        let input = "if () {}";
+        Parser::new(input.into());
     }
 }
