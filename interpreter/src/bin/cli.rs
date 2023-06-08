890cb6694f0e7e97e35da39149f8f224a06d1bd9
tavindev <gvwchevrand@gmail.com>
2023-06-08 08:29:26 +0100

    binaries cleanup

--- interpreter/src/bin/cli.rs
+++ interpreter/src/bin/cli.rs
@@ -1,9 +1,7 @@
-use std::{cell::RefCell, rc::Rc};
+use std::rc::Rc;
 
-use js_interpreter_in_rust::{
-    interpreter::{environment::Environment, interpreter::Interpreter},
-    parser::parser::Parser,
-};
+use interpreter::{environment::Environment, interpreter::Interpreter};
+use parser::parser::Parser;
 
 fn main() {
     let path = std::env::args().nth(1).expect("missing path argument");
@@ -11,8 +9,8 @@ fn main() {
     let mut parser = Parser::new(source);
     let statements = parser.parse();
 
-    let mut environment = Rc::new(RefCell::new(Environment::new()));
+    let environment = Rc::new(Environment::new());
     let mut intepreter = Interpreter::new(statements);
 
-    intepreter.run(&mut environment);
+    intepreter.run(&environment);
 }
